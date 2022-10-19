5eed7428e8daad88f54e696fcab7593a03a6b0b2
n4beel <nabeel3353045805@hotmail.com>
2022-10-19 23:26:16 +0500

    log updated

--- pages/api/log.ts
+++ pages/api/log.ts
@@ -11,7 +11,7 @@ export default function handler(
 ) {
   if (req.method === "POST") {
     // Process a POST request
-    console.log(req.body.log);
+    console.log(req.body);
     res.status(200).json({ message: "logged successsfully" });
   } else {
     // Handle any other HTTP method
